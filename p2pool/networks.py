da39f78d3bcb6f9d2c6f84724eaf0fd6473269ba
makishart <makishart@users.noreply.github.com>
2014-02-25 23:32:18 +0200

    Update networks.py

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -160,6 +160,25 @@ nets = dict(
         ANNOUNCE_CHANNEL='#p2pool-panda',
         VERSION_CHECK=lambda v: True,
     ),
+    suncoin=math.Object(
+        PARENT=networks.nets['suncoin'],
+        SHARE_PERIOD=12, # seconds
+        CHAIN_LENGTH=24*60*60//10, # shares
+        REAL_CHAIN_LENGTH=24*60*60//10, # shares
+        TARGET_LOOKBEHIND=200, # shares
+        SPREAD=30, # blocks
+        IDENTIFIER='b41b2356a5b7d69d'.decode('hex'),
+        PREFIX='1623b92172d2b5a1'.decode('hex'),
+        P2P_PORT=9189,
+        MIN_TARGET=4,
+        MAX_TARGET=2**256//2**20 - 1,
+        PERSIST=False,
+        WORKER_PORT=9191,
+        BOOTSTRAP_ADDRS='sun.gr2pool.eu'.split(' '),
+        ANNOUNCE_CHANNEL='#gr2pool #suncoin',
+        VERSION_CHECK=lambda v: True,
+    ),    
+    
 )
 for net_name, net in nets.iteritems():
     net.NAME = net_name
