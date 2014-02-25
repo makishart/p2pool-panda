177743cfe8ff45aa5ad16da6fb945efd29994c28
makishart <makishart@users.noreply.github.com>
2014-02-25 23:32:54 +0200

    Update networks.py

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -251,6 +251,30 @@ nets = dict(
         DUMB_SCRYPT_DIFF=2**16,
         DUST_THRESHOLD=0.03e8,
     ),
+    
+    suncoin=math.Object(
+        P2P_PREFIX='fcd9b7dd'.decode('hex'),
+        P2P_PORT=9296,
+        ADDRESS_VERSION=63,
+        RPC_PORT=9295,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'suncoin address' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 10*92955807,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=60, # s
+        SYMBOL='SUN',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'suncoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/suncoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.suncoin'), 'suncoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://explorer.suncoin.biz/index.php?block_hash=',
+        ADDRESS_EXPLORER_URL_PREFIX='http://explorer.suncoin.biz/',
+        TX_EXPLORER_URL_PREFIX='http://explorer.suncoin.biz/index.php?transaction=',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+        DUST_THRESHOLD=0.03e8,
+    ),
+
+    
 
 )
 for net_name, net in nets.iteritems():
